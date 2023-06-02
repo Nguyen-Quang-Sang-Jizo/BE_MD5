2ab1da51d2bd0bb8c3e460810ddd7fc8694ead14
unknown <leong.dev.35@gmail.com>
2023-06-02 16:56:21 +0700

    update

--- dist/src/configs/data-source.d.ts
+++
@@ -1,5 +0,0 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-declare const AppDataSource: DataSource;
-declare function connectDB(): Promise<void>;
-export { connectDB, AppDataSource };
